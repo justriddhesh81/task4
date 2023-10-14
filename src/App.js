8413ce736cdca19f821dc4b836465e6b81dddc9c
Riddhesh <riddheshkarnik21@gmail.com>
2023-10-15 00:04:06 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Details from "./Components/records_task.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Details />
+    </div>
+  );
+}
+
+export default App;
